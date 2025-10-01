99e87b4b60fcda8e537478060dd373c841103bd8
PinkMan1g <77533479+PinkMan1g@users.noreply.github.com>
2025-10-01 16:06:02 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
       <motion.div {...scrollAnimation} className="max-w-2xl text-center z-10">
         <h2 className="text-3xl sm:text-4xl font-bold mb-6 text-white font-retro">About Me</h2>
         <p className="text-xl sm:text-3xl text-white font-retro">
-          I&apos;ve been developing Minecraft content for over five years now. In that time, I&apos;ve worked with multiple teams and studios, creating a wide range of add-ons and dynamic worlds for the Marketplace. I really enjoy bringing ideas to life through scripting and custom gameplay mechanics.  <br /><br />I have solid experience with entities, block components, and building custom mechanics. I also work a lot with particles and custom sounds to obtain better user experience. My goal is always to keep things clean, functional and to deliver on time.  <br /><br /> I regularly use GitHub and Jira to manage projects and maintain clear workflows. 
+          I&apos;ve been developing Minecraft content for over five years now. In that time, I&apos;ve worked with multiple teams and studios, creating a wide range of add-ons and dynamic worlds for the Marketplace. I really enjoy bringing ideas to life through scripting and custom gameplay mechanics.  <br /><br />I have solid experience with entities, block components, and building custom mechanics. I also work a lot with particles and custom sounds to obtain better user experience. My goal is always to keep things clean, functional and to deliver on time.  <br /><br /> I regularly use GitHub, Notion and Jira to manage projects and maintain clear workflows. 
         </p>
       </motion.div>
     </section>
